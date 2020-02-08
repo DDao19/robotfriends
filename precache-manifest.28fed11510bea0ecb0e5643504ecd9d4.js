80426581cb0f5fc062a6d3258dba8bb6cac06704
Dung <dung.dao19@outlook.com>
2020-02-07 22:59:00 -0600

    Updates

---
+++ precache-manifest.28fed11510bea0ecb0e5643504ecd9d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a231c20c4c3f908cd1e7b0bb60b0c1e",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "037c229f7ba2ec85306a",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "785ef3276db440cfcbb8",
+    "url": "/robotfriends/static/css/main.e81cdc77.chunk.css"
+  },
+  {
+    "revision": "037c229f7ba2ec85306a",
+    "url": "/robotfriends/static/js/2.75d9c582.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotfriends/static/js/2.75d9c582.chunk.js.LICENSE"
+  },
+  {
+    "revision": "785ef3276db440cfcbb8",
+    "url": "/robotfriends/static/js/main.90710762.chunk.js"
+  },
+  {
+    "revision": "8432a6681c6fc45d179d",
+    "url": "/robotfriends/static/js/runtime-main.05482e96.js"
+  }
+]);
